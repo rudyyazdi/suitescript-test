14bb7ca465b4c350fcc96c114b83b7f49e008867
rudyyazdi <rudyyazdi@gmail.com>
2016-09-22 14:45:02 +1000

    added shotcut for debugger

--- debugger.js
+++ debugger.js
@@ -1,3 +1,12 @@
 require(["N/log"], function(log) {
   log.debug('success!');
 });
+
+
+// -------  OR:
+
+define = function(arr, fun) { return require(arr, fun) };
+
+define(["N/log"], function(log) {
+  log.debug('success!');
+});
